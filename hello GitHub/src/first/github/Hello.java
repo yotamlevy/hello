160c9df6b0578150be2b861f0a197bdb73aa5b8f
Yotam Levy <pfyotam@gmail.com>
2016-04-24 01:12:03 +0200

    Added files via upload

--- hello GitHub/src/first/github/Hello.java
+++ hello GitHub/src/first/github/Hello.java
@@ -3,7 +3,8 @@ package first.github;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("hello gitHub");
+		System.out.println("hello gitHub\n");
+		System.out.println("*some change*");
 
 	}
 
